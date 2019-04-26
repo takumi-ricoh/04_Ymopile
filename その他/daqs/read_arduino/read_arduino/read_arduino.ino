3343d88fb02e4e23a1e2dbb892131267a2632280
takumi <takumi.waida@nts.ricoh.co.jp>
2019-04-26 18:28:17 +0900

    arduino

---
+++ その他/daqs/read_arduino/read_arduino/read_arduino.ino
@@ -0,0 +1,100 @@
+
+//    2018/11/22
+//
+//   複数のセンサの検出温度（Tobj）を表示
+//　 センサのアドレスは、0x00からインクリメント
+//
+//   センサには、0x01からアドレスを振っておくこと。
+//   1chあたりの測定時間は、2[ms]。127個読むのには約254[ms]要する。
+//   センサのデータ更新時間が100[ms]のため、同じセンサを読み出すのは100[ms]以上経過後にする処理を入れてある。
+//   どのセンサの出力値かわかるように、データの前にセンサ番号を追加。
+
+#include <Wire.h>
+
+#define WAIT 100            //Wait時間（同一センサを読む時の間隔が、100[ms]以上になるようにする）
+#define SENSOR_NUM 24       //センサ数
+#define SENSOR_BASEADR 0x5A-1 //センサのベースアドレス
+
+unsigned long previousMillis = millis();
+unsigned long time;
+
+/**************/
+/*** 実行部 ***/
+/**************/
+void setup() {
+  //I2Cスタート
+  Serial.begin(115200);    // ターミナルソフトのボーレートは115200に指定すること
+  Wire.begin();
+  //  pinMode(8, OUTPUT);
+  delay(200);
+}
+
+void loop() {
+  unsigned int i;
+  unsigned long currentMillis = millis();
+  unsigned long dt = currentMillis - previousMillis;
+  if (dt > WAIT) {        //一つ目のセンサを読むタイミングが、前回測定時より間隔が、WAIT[ms]以上になら測定開始
+    previousMillis = currentMillis;
+
+    time = millis();
+
+    //時刻の表示
+    //Serial.print(time);
+    //Serial.print( ":" );
+
+    //ダミー熱電対データ
+    //Serial.print("0,0,");
+
+    for ( i = 1; i < SENSOR_NUM+1; i++ ) {          
+      // 全部のセンサの測定値を順次読み出す
+      double Tobj = readTobj( SENSOR_BASEADR + i );
+      double Tamb = readTamb( SENSOR_BASEADR + i );
+
+      if( Tobj > 0.0 ) {                          // 接続されているセンサの結果だけ出力（接続されていないセンサからの戻り値は0[deg]）
+        Serial.print( i );                        // sensor ID
+        Serial.print( "," );                     // 結果を出力する
+        Serial.print("0,0,");                     //sensor data // reserved
+        Serial.print( Tobj );                    // obj temp
+        Serial.print( "," );                     // 結果を出力する
+        Serial.print( Tamb );                    // sensor temp
+        Serial.print(';');                       // 区切り文字を出力する
+      }     
+    }
+  Serial.println();                              // 改行を出力する
+  }
+}
+
+/********************/
+/*** 温度換算関数 ***/
+/********************/
+double readTobj( uint8_t sen_adr ) {
+  int16_t raw = read16_repeat( 0x07, sen_adr );
+  double Tobj = raw * 0.02 - 273.15;
+  if ( Tobj < 0 ) Tobj = 0;
+  return Tobj;
+}
+
+double readTamb( uint8_t sen_adr ) {
+  int16_t raw = read16_repeat( 0x06, sen_adr );
+  double Tobj = raw * 0.02 - 273.15;
+  if ( Tobj < 0 ) Tobj = 0;
+  return Tobj;
+}
+
+/****************/
+/*** I2C 関数 ***/
+/****************/
+uint16_t read16_repeat( uint8_t reg_adr, uint8_t sen_adr ) {
+  uint16_t ret;
+
+  Wire.beginTransmission( sen_adr ); // start transmission to device
+  Wire.write( reg_adr ); // sends register address to read from
+  Wire.endTransmission( false ); // end transmission
+
+  Wire.requestFrom( sen_adr, (uint8_t)3);// send data n-bytes read
+  ret = Wire.read(); // receive DATA
+  ret |= Wire.read() << 8;
+  Wire.endTransmission( true );
+  return ret;
+}
+
